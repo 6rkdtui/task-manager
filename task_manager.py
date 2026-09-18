c6158ba5c2afdfa735237c2c5e3ff168e76389d3
master_user <master_user@gmail.com>
2026-09-18 18:56:40 +0300

    Add SQLite support for task creation and retrieval

--- task_manager.py
+++ task_manager.py
@@ -1,22 +1,37 @@
 from task import Task
+import sqlite3
 
 
 class TaskManager:
-    def __init__(self) -> None:
-        self.tasks: dict[int, Task] = {}
-        self._next_id: int = 1
+    def __init__(self, db_name: str) -> None:
+        self.db_name = db_name
+        self.connection = sqlite3.connect(self.db_name)
 
     def add_task(self, title: str, description: str) -> Task:
-        task = Task(self._next_id, title, description)
-        self.tasks[self._next_id] = task
-        self._next_id += 1
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "INSERT INTO tasks (title, description) VALUES (?, ?)", (title, description)
+        )
+        self.connection.commit()
+
+        new_id = cursor.lastrowid
+        task = Task(new_id, title, description)
+
+        cursor.close()
         return task
 
     def get_task(self, task_id: int) -> Task | None:
-        if task_id in self.tasks:
-            return self.tasks[task_id]
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "SELECT id, title, description, is_completed FROM tasks WHERE id = ?",
+            (task_id,),
+        )
+        task = cursor.fetchone()
+        cursor.close()
+        if task is None:
+            return None
 
-        return None
+        return Task(task[0], task[1], task[2], bool(task[3]))
 
     def get_all_tasks(self) -> list[Task]:
         return list(self.tasks.values())
